92902f0d7056e2084f06284935b5b523602e296c
mradalbert <mister.adalbert@gmail.com>
2017-06-17 20:54:24 +0200

    highscore upload functional1ty

--- src/BomberManServer/ServerActions.java
+++
@@ -1,8 +0,0 @@
-package BomberManServer;
-
-/**
- * Created by Wojtek on 20.05.2017.
- */
-public class ServerActions {
-
-}
--- src/BomberManServer/ServerThread.java
+++ src/BomberManServer/ServerThread.java
@@ -27,8 +27,12 @@ public class ServerThread extends Thread {
                 BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
                 String clientRequest = input.readLine();
-                if(clientRequest == null)
+
+
+                if (clientRequest == null)//cotokurwajest
                     break;
+
+
                 String[] clientCommands = clientRequest.split(",");
                 System.out.println(socket.getInetAddress() + " " + clientRequest);
                 switch (clientCommands[0]) {
@@ -42,9 +46,29 @@ public class ServerThread extends Thread {
                         serverMessage = "GAMECONFIG," + server.gameconfig;
                         break;
                     case "GET_MAP":
-                        serverMessage = "MAP," + server.maps.get(Integer.valueOf(clientCommands[1])-1);
+                        serverMessage = "MAP," + server.maps.get(Integer.valueOf(clientCommands[1]) - 1);
                         break;
                     case "SET_HIGHSCORE":
+                        String[] temp = server.highScores.split(",");
+                        for (int i = 0; i < 10; i++) {
+                            if (Integer.valueOf(temp[2 * i + 1]) < Integer.valueOf(clientCommands[2])) {
+                                for (int j = i + 1; j < 10; j++) {
+                                    temp[2 * j] = temp[2 * j - 2];
+                                    temp[2 * j + 1] = temp[2 * j - 1];
+                                }
+                                temp[2 * i] = clientCommands[1];
+                                temp[2 * i + 1] = clientCommands[2];
+
+                                server.highScores = temp[0] + "," + temp[1];
+                                for (int j = 1; j < 10; j++)
+                                    server.highScores += "," + temp[2 * j] + "," + temp[2 * j + 1];
+
+                                server.highScores += "\n";
+                                break;
+                            }
+                        }
+                    default:
+                        serverMessage = "SERVER_PANIC 600\n";
                 }
 
                 outputStream.writeBytes(serverMessage);
